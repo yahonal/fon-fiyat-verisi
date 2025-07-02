720619e45c948bffe221255d291a37d4f5fcbb26
Yahya Onur Altın <onur.altin@roketsan.com.tr>
2025-07-02 13:14:55 +0300

    otomasyon loglar silindi

--- fon-fiyat-verisi/otomatik_yayin_calistir.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-echo [INFO] Otomatik yayin scripti baslatiliyor...
-python otomatik_yayin_final_guncel.py
-
-echo.
-echo [INFO] Islemler tamamlandi. Cikmak icin bir tusa basin...
-pause >nul
